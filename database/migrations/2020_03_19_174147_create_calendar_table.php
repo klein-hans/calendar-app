c920a969ebd22769426862197e242463e5a01eb4
unknown <Klein-Hans>
2020-03-20 01:53:05 +0800

    calendar table migration

---
+++ database/migrations/2020_03_19_174147_create_calendar_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCalendarTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('calendar', function (Blueprint $table) {
+            $table->id();
+            $table->string('event_name');
+            $table->date('event_date');
+            $table->softDeletes();
+            $table->string('created_by');
+            $table->string('updated_by');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('calendar');
+    }
+}
