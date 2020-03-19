267e1587cbafeddfb3584f11fa1f8a370be5c3f6
unknown <Klein-Hans>
2020-03-20 02:19:58 +0800

    assets

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons" />
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
-    <link rel="stylesheet" href="/laravel-test/public/css/app.css">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <script src="https://code.jquery.com/jquery-3.4.1.js" integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU=" crossorigin="anonymous"></script>
     <script src='https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js' type="text/javascript"></script>
   </head>
@@ -17,5 +17,5 @@
       @yield('content')
     </div>
   </body>
-  <script src='/laravel-test/public/js/app.js' type="text/javascript"></script>
+  <script src="{{ asset('js/app.js') }}" type="text/javascript"></script>
 </html>
